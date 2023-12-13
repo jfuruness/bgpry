b96a5b085df9914439b222a420421620b872c3c8
Justin Furuness <jfuruness@gmail.com>
2023-12-13 01:08:30 -0500

    added rust

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-# NOTE to self: leave this here since
-# you will end up using this for Rust bindings I think
 from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        name="bgpry",
+        version="3.0.0",
+        rust_extensions=[
+            RustExtension("bgpry.bgpr", "bgpr/Cargo.toml", binding=Binding.PyO3)],
+        packages=["bgpry"],
+        zip_safe=False,
+    )
